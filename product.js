c48490f0cbd3780950c2aaaba47d41655220461a
Evaan Pallimalil <113658319+evaanpallimalil@users.noreply.github.com>
2022-10-07 20:26:36 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,25 @@
+let clockk= document.getElementById('ind-not')
+
+let num=document.getElementById('ind-not')
+
+function fin(){
+    num.innerText=0
+}
+
+// function doodle(){
+// }
+
+
+
+function doodle(){
+    document.querySelector('.web1').style.backgroundColor='white'
+    
+}
+function doo(){
+document.querySelector('.web2').style.backgroundColor='white'
+}
+
+
+
+
+
