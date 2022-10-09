3842a071dc0a6b90f8385bb60054ac4007fa8469
Evaan Pallimalil <113658319+evaanpallimalil@users.noreply.github.com>
2022-10-09 08:47:02 +0530

    Add files via upload

--- product.js
+++ product.js
@@ -4,8 +4,10 @@ let num=document.getElementById('ind-not')
 
 function fin(){
     num.innerText=0
-}
+    document.querySelector('.web1').style.backgroundColor='white'
+    document.querySelector('.web2').style.backgroundColor='white'
 
+}
 function doodle(){
     document.querySelector('.web1').style.backgroundColor='white'
 }
@@ -13,6 +15,7 @@ function doodle(){
 function doo(){
 document.querySelector('.web2').style.backgroundColor='white'
 }
+
 
 
 
